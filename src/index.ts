f72573fc2abe8e03e01d4010424cf78028caf705
Kenzie Bottoms <kenziebottoms@gmail.com>
2023-08-31 11:21:04 -0500

    interface refactor

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,14 @@
+import { CsvFileReader } from './CsvFileReader';
 import { MatchReader } from './MatchReader';
 import { MatchResult } from './MatchResult';
 
-const reader = new MatchReader('football.csv');
-reader.read();
-const matches = reader.data;
+const csvReader = new CsvFileReader('football.csv');
+const matchReader = new MatchReader(csvReader);
+matchReader.load();
 
 let manUnitedWins = 0;
 
-for (let match of matches) {
+for (let match of matchReader.matches) {
   if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
     manUnitedWins++;
   } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
