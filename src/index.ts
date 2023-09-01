6f267abd0dabbaea3e9dfc5ff6c396304ad00f14
Kenzie Bottoms <kenziebottoms@gmail.com>
2023-09-01 16:07:09 -0500

    report static method

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,10 @@
 import { CsvFileReader } from './CsvFileReader';
 import { MatchReader } from './MatchReader';
-import { HtmlReport } from './reportTargets/HtmlReport';
-import { WinsAnalysis } from './analyzers/WinsAnalysis';
 import { Summary } from './Summary';
 
 const csvReader = new CsvFileReader('football.csv');
 const matchReader = new MatchReader(csvReader);
 matchReader.load();
 
-const summary = new Summary(new WinsAnalysis('Man United'), new HtmlReport());
+const summary = Summary.winsAnalysisWithHtmlReport('Man United');
 summary.buildAndPrintReport(matchReader.matches);
